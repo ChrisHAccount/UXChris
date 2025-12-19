eea60ff9a23fa4c4f0ab4c3d6a109f05bc2a0989
Chris H <alias4444@icloud.com>
2025-12-19 10:55:27 +0000

    Updated app.js to include direction to index.html as landing page

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { CosmosClient } = require('@azure/cosmos');
+const path = require('path');
+
+// Create the Express app & define port
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware - Parse incoming JSON for POST requests
+app.use(express.json());
+
+// Middleware - Serve static files (HTML, CSS, JS) from the 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve index.html as homepage
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+   });   
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+   });
